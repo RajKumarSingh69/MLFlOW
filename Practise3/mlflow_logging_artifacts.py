6f4b1ba6b9a421e6ae4cab99eb02e2007dd2cf24
RajKumarSingh69 <124248002+RajKumarSingh69@users.noreply.github.com>
2024-07-09 18:27:46 +0530

    Add files via upload

---
+++ Practise3/mlflow_logging_artifacts.py
@@ -0,0 +1,54 @@
+import mlflow
+from mlflow_utils import get_mlflow_experiment
+
+'''if __name__=="__main__":
+    experiment = get_mlflow_experiment(experiment_name="testing_mlflow1")
+
+    print("Name: {}".format(experiment.name))
+
+    with mlflow.start_run(run_name="logging_artifacts", experiment_id=experiment.experiment_id) as run:
+
+        # your machine learning code goes here
+
+        # create a text file that says hello world
+        with open("hello_world.txt", "w") as f:
+            f.write("Hello World!")
+
+        # log the text file as an artifact
+        mlflow.log_artifact(local_path="hello_world.txt", artifact_path="text_files")
+
+        # print run info
+        print("run_id: {}".format(run.info.run_id))
+        print("experiment_id: {}".format(run.info.experiment_id))
+        print("status: {}".format(run.info.status))
+        print("start_time: {}".format(run.info.start_time))
+        print("end_time: {}".format(run.info.end_time))
+        print("lifecycle_stage: {}".format(run.info.lifecycle_stage))'''
+
+
+
+#2nd method for logging the artifacts
+import mlflow
+from mlflow_utils import get_mlflow_experiment
+
+if __name__=="__main__":
+
+    experiment = get_mlflow_experiment(experiment_name="testing_mlflow1")
+
+    print("Name: {}".format(experiment.name))
+
+    with mlflow.start_run(run_name="logging_artifacts", experiment_id=experiment.experiment_id) as run:
+
+        # your machine learning code goes here
+        #artifact folder path
+        path=r"F:\DATA_SCIENCE\MLOPS\MLFLOW\Practise3\run_artifacts"
+        #here we are logging a artifact folder where all the things are present
+        mlflow.log_artifacts(local_dir=path,artifact_path="run_artifacts")
+
+        # print run info
+        print("run_id: {}".format(run.info.run_id))
+        print("experiment_id: {}".format(run.info.experiment_id))
+        print("status: {}".format(run.info.status))
+        print("start_time: {}".format(run.info.start_time))
+        print("end_time: {}".format(run.info.end_time))
+        print("lifecycle_stage: {}".format(run.info.lifecycle_stage)) 
